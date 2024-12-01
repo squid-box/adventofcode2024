f0aae5c536bc577bd757376575be193d6f3fede0
Joel Ahlgren <joel@ahlgren.io>
2024-12-01 07:54:54 +0100

    Solved Day 1.

--- Source/AdventOfCode2024.Tests/Problems/Problem1Tests.cs
+++ Source/AdventOfCode2024.Tests/Problems/Problem1Tests.cs
@@ -1,11 +1,31 @@
-namespace AdventOfCode2024.Tests.Problems
-{
-    using AdventOfCode2024.Problems;
-    using NUnit.Framework;
+namespace AdventOfCode2024.Tests.Problems;
 
-    [TestFixture]
-    public class Problem1Tests
+using AdventOfCode2024.Problems;
+using NUnit.Framework;
+
+[TestFixture]
+public class Problem1Tests
+{
+    private static readonly string[] TestInput =
+    [
+        "3   4",
+        "4   3",
+        "2   5",
+        "1   3",
+        "3   9",
+        "3   3"
+    ];
+
+    [Test]
+    public void TestPartOne()
+    {
+        Assert.That(Problem1.PartOne(TestInput), Is.EqualTo(11));
+    }
+
+    [Test]
+    public void TestPartTwo()
     {
 
+        Assert.That(Problem1.PartTwo(TestInput), Is.EqualTo(31));
     }
 }
--- Source/AdventOfCode2024/Problems/Problem1.cs
+++ Source/AdventOfCode2024/Problems/Problem1.cs
@@ -1,20 +1,75 @@
-namespace AdventOfCode2024.Problems
+namespace AdventOfCode2024.Problems;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2024.Utils.Extensions;
+
+/// <summary>
+/// Solution for <a href="https://adventofcode.com/2024/day/1">Day 1</a>.
+/// </summary>
+public class Problem1(InputDownloader inputDownloader) : ProblemBase(1, inputDownloader)
 {
-    /// <summary>
-    /// Solution for <a href="https://adventofcode.com/2024/day/1">Day 1</a>.
-    /// </summary>
-    public class Problem1(InputDownloader inputDownloader) : ProblemBase(1, inputDownloader)
+    /// <inheritdoc />
+    protected override object SolvePartOne()
     {
-        /// <inheritdoc />
-        protected override object SolvePartOne()
+        return PartOne(Input);
+    }
+
+    /// <inheritdoc />
+    protected override object SolvePartTwo()
+    {
+        return PartTwo(Input);
+    }
+
+    public static int PartOne(IEnumerable<string> input)
+    {
+        var leftList = new List<int>();
+        var rightList = new List<int>();
+
+        foreach (var line in input)
         {
-            return "Unsolved";
+            var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            leftList.Add(split[0].ToInt());
+            rightList.Add(split[1].ToInt());
         }
 
-        /// <inheritdoc />
-        protected override object SolvePartTwo()
+        leftList.Sort();
+        rightList.Sort();
+
+        var sumOfDifferences = 0;
+
+        for (var i = 0; i < leftList.Count; i++)
         {
-            return "Unsolved";
+            sumOfDifferences += Math.Abs(leftList[i] - rightList[i]);
         }
+
+        return sumOfDifferences;
+    }
+
+    public static long PartTwo(IEnumerable<string> input)
+    {
+        var leftList = new List<int>();
+        var rightList = new List<int>();
+
+        foreach (var line in input)
+        {
+            var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            leftList.Add(split[0].ToInt());
+            rightList.Add(split[1].ToInt());
+        }
+
+        var sumOfSimilarityScores = 0;
+
+        foreach (var leftNumber in leftList)
+        {
+            var rightListCount = rightList.Count(n => n == leftNumber);
+
+            sumOfSimilarityScores += rightListCount * leftNumber;
+        }
+
+        return sumOfSimilarityScores;
     }
 }
