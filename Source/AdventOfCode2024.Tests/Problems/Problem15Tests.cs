534cfc6bf2d06d9348506db77bff38497a3845c9
Joel Ahlgren <joel@ahlgren.io>
2024-12-15 23:05:16 +0100

    Solved Day 15, part 1.

--- Source/AdventOfCode2024.Tests/Problems/Problem15Tests.cs
+++ Source/AdventOfCode2024.Tests/Problems/Problem15Tests.cs
@@ -6,20 +6,55 @@ using NUnit.Framework;
 [TestFixture]
 public class Problem15Tests
 {
-    private static readonly string[] TestInput =
+    private static readonly string[] TestInput1 =
     [
-        
+        "########",
+        "#..O.O.#",
+        "##@.O..#",
+        "#...O..#",
+        "#.#.O..#",
+        "#...O..#",
+        "#......#",
+        "########",
+        "",
+        "<^^>>>vv<v>>v<<"
+    ];
+
+    private static readonly string[] TestInput2 =
+    [
+        "##########",
+        "#..O..O.O#",
+        "#......O.#",
+        "#.OO..O.O#",
+        "#..O@..O.#",
+        "#O#..O...#",
+        "#O..O..O.#",
+        "#.OO.O.OO#",
+        "#....O...#",
+        "##########",
+        "",
+        "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+        "vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+        "><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+        "<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+        "^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+        "^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+        ">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+        "<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+        "^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+        "v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"
     ];
 
     [Test]
     public void TestPartOne()
     {
-        Assert.That(Problem15.PartOne(TestInput), Is.EqualTo(4711));
+        Assert.That(Problem15.PartOne(TestInput1), Is.EqualTo(2028));
+        Assert.That(Problem15.PartOne(TestInput2), Is.EqualTo(10092));
     }
 
     [Test]
     public void TestPartTwo()
     {
-        Assert.That(Problem15.PartTwo(TestInput), Is.EqualTo(4711));
+        Assert.That(Problem15.PartTwo(TestInput2), Is.EqualTo(9021));
     }
 }
--- Source/AdventOfCode2024/Problems/Problem15.cs
+++ Source/AdventOfCode2024/Problems/Problem15.cs
@@ -1,6 +1,10 @@
 namespace AdventOfCode2024.Problems;
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2024.Utils;
+using AdventOfCode2024.Utils.Extensions;
 
 /// <summary>
 /// Solution for <a href="https://adventofcode.com/2024/day/15">Day 15</a>.
@@ -21,11 +25,249 @@ public class Problem15(InputDownloader inputDownloader) : ProblemBase(15, inputD
 
     public static object PartOne(IEnumerable<string> input)
     {
-        return "Unsolved";
+        var inputChunks = input.ToList().SplitByBlankLines();
+
+        var warehouse = new Warehouse(inputChunks[0]);
+        var instructions = string.Join("", inputChunks[1]);
+
+        foreach (var c in instructions)
+        {
+            warehouse.MoveRobot(c);
+        }
+
+        return warehouse.Map.GetElements()
+            .Where(tuple => tuple.Element == 'O')
+            .Select(tuple => tuple.Position)
+            .Sum(CalculateGpsCoordinate);
     }
 
     public static object PartTwo(IEnumerable<string> input)
     {
-        return "Unsolved";
+        var inputChunks = input.ToList().SplitByBlankLines();
+
+        var warehouse = new Warehouse(inputChunks[0], wide: true);
+        var instructions = string.Join("", inputChunks[1]);
+
+        foreach (var c in instructions)
+        {
+            warehouse.MoveWideRobot(c);
+        }
+
+        warehouse.Map[warehouse.Robot] = '@';
+        Console.WriteLine(warehouse.Map);
+
+        return warehouse.Map.GetElements()
+            .Where(tuple => tuple.Element == '[')
+            .Select(tuple => tuple.Position)
+            .Sum(CalculateGpsCoordinate);
+    }
+
+    private class Warehouse
+    {
+        public Warehouse(IList<string> input, bool wide = false)
+        {
+            var height = input.Count;
+            var width = input.First().Length;
+
+            Map = new Matrix<char>(width * (wide ? 2 : 1) + (wide ? 1 : 0), height);
+
+            for (var y = 0; y < height; y++)
+            {
+                var line = input[y];
+
+                var newLine = "";
+
+                foreach (var c in line)
+                {
+                    if (c == '#')
+                    {
+                        newLine += "##";
+                    }
+                    else if (c == '@')
+                    {
+                        newLine += "@.";
+                    }
+                    else if (c == '.')
+                    {
+                        newLine += "..";
+                    }
+                    else if (c == 'O')
+                    {
+                        newLine += "[]";
+                    }
+                }
+
+                line = newLine;
+
+                for (var x = 0; x < line.Length; x++)
+                {
+                    var current = line[x];
+
+                    if (current == '@')
+                    {
+                        Robot = new Coordinate(x, y);
+                        Map[x, y] = '.';
+                    }
+                    else
+                    {
+                        Map[x, y] = current;
+                    }
+                }
+            }
+        }
+
+        public Matrix<char> Map { get; }
+
+        public Coordinate Robot { get; private set; }
+
+        public void MoveRobot(char instruction)
+        {
+            // Determine where we're going...
+            var direction = instruction switch
+            {
+                '<' => Vector.West,
+                '>' => Vector.East,
+                'v' => Vector.South,
+                '^' => Vector.North,
+                _ => throw new Exception($"Unexpected instruction: '{instruction}'.")
+            };
+
+            var nextPosition = Robot + direction;
+            var boxesToMove = new List<Coordinate>();
+            var foundWall = false;
+
+            // Check what's in that direction...
+            while (Map.IsWithinBounds(nextPosition))
+            {
+                var nextTile = Map[nextPosition];
+
+                if (nextTile == '.')
+                {
+                    // We're at an empty space!
+                    break;
+                }
+
+                if (nextTile == '#')
+                {
+                    foundWall = true;
+                    break;
+                }
+
+                if (nextTile == 'O')
+                {
+                    boxesToMove.Add(nextPosition);
+                }
+
+                nextPosition += direction;
+            }
+
+            if (!foundWall)
+            {
+                boxesToMove.Reverse();
+
+                foreach (var box in boxesToMove)
+                {
+                    Map[box] = '.';
+                    Map[box + direction] = 'O';
+                }
+
+                Robot += direction;
+            }
+        }
+
+        public void MoveWideRobot(char instruction)
+        {
+            // Determine where we're going...
+            var direction = instruction switch
+            {
+                '<' => Vector.West,
+                '>' => Vector.East,
+                'v' => Vector.South,
+                '^' => Vector.North,
+                _ => throw new Exception($"Unexpected instruction: '{instruction}'.")
+            };
+
+            var nextPosition = Robot + direction;
+            var boxesToMove = new List<Coordinate>();
+            var foundWall = false;
+
+            // Check what's in that direction...
+            while (Map.IsWithinBounds(nextPosition))
+            {
+                var nextTile = Map[nextPosition];
+
+                if (nextTile == '.')
+                {
+                    // We're at an empty space!
+                    break;
+                }
+
+                if (nextTile == '#')
+                {
+                    foundWall = true;
+                    break;
+                }
+
+                if (nextTile == '[')
+                {
+                    boxesToMove.Add(nextPosition);
+                    boxesToMove.Add(nextPosition + Vector.East);
+                }
+                else if (nextTile == ']')
+                {
+                    boxesToMove.Add(nextPosition);
+                    boxesToMove.Add(nextPosition + Vector.West);
+                }
+
+                // TODO: if we're moving on the Y-axis, we need to do a lot more collision/pushing checks...
+
+                nextPosition += direction;
+            }
+
+            if (!foundWall)
+            {
+                boxesToMove.Reverse();
+
+                var boxesMoved = new HashSet<Coordinate>();
+
+                foreach (var box in boxesToMove)
+                {
+                    if (boxesMoved.Contains(box))
+                    {
+                        continue;
+                    }
+
+                    if (Map[box] == '[')
+                    {
+                        Map[box] = '.';
+                        Map[box + Vector.East] = '.';
+
+                        Map[box + direction] = '[';
+                        Map[box + Vector.East + direction] = ']';
+
+                        boxesMoved.Add(box);
+                        boxesMoved.Add(box + Vector.East);
+                    }
+                    else
+                    {
+                        Map[box] = '.';
+                        Map[box + Vector.West] = '.';
+
+                        Map[box + direction] = ']';
+                        Map[box + Vector.West + direction] = '[';
+
+                        boxesMoved.Add(box);
+                        boxesMoved.Add(box + Vector.West);
+                    }
+                }
+
+                Robot += direction;
+            }
+        }
+    }
+
+    private static long CalculateGpsCoordinate(Coordinate box)
+    {
+        return (box.Y * 100) + box.X;
     }
 }
